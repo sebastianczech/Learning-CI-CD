40eaf20f5826282f48cba881674e67eaad52436a
Sebastian Czech <sebaczech@gmail.com>
2020-08-17 23:15:01 +0200

    Docker installation

--- playbooks/cicd.sh
+++ playbooks/cicd.sh
@@ -1,3 +1,4 @@
 # ansible-playbook -i hosts.yml playbook-check-connection.yml
 # ansible-playbook -i hosts.yml playbook-zsh.yml
-ansible-playbook -i hosts.yml playbook-screen.yml
+# ansible-playbook -i hosts.yml playbook-screen.yml
+# ansible-playbook -i hosts.yml playbook-docker.yml
